c3b6d6693981716f6084a680f04643989ead9753
mgalla16 <31961852+mgalla16@users.noreply.github.com>
2018-02-22 04:27:03 -0800

    Final code

--- fakenews.py
+++ fakenews.py
@@ -5,24 +5,49 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
+from nltk.stem.snowball import SnowballStemmer
+import random
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
-vectorizer = TfidfVectorizer(min_df=3, stop_words='english',ngram_range=(1, 2))
-X = vectorizer.fit_transform(train['text'].values.astype('U'))
+train = pd.read_csv('train.csv')
+train = train.fillna(value="NA")
+merged = []
+for row in train.itertuples():
+    merged.append(" ".join(row[2:5]))
+train['merged'] = merged
+stems = []
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
+for row in train.itertuples():
+    body = row[-1].split()
+    words = [stemmer.stem(word) for word in body]
+    stems.append(" ".join(words))
+train["Stems"] = stems
+print(train.head())
+
+vectorizer = TfidfVectorizer(min_df=6, stop_words='english', ngram_range=(1, 5))
+X = vectorizer.fit_transform(train['Stems'].values.astype('U'))
 y = np.array(train['label'])
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, stratify=y, random_state=1234)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, stratify=y, random_state=random.randint(1000, 9999))
+lr = LogisticRegression(penalty='l2', solver='newton-cg', C=1000000)
+model = lr.fit(X_train,y_train)
+print(model.score(X_test,y_test))
 
-lr = LogisticRegression(penalty='l2',C=1, solver='lbfgs', multi_class='multinomial')
-lr.fit(X_train,y_train)
-print("Logistic Regression: ", end="")
-print(lr.score(X_test,y_test))
+test = pd.read_csv('test.csv')
+test = test.fillna(value = "NA")
+merged_test = []
+for row in test.itertuples():
+    merged_test.append(" ".join(row[2:5]))
+test['merged'] = merged_test
+stems = []
+for row in test.itertuples():
+    body = row[-1].split()
+    words = [stemmer.stem(word) for word in body]
+    stems.append(" ".join(words))
+test["Stems"] = stems
 
-X_test = vectorizer.transform(test['text'].values.astype('U'))
-lr_preds = lr.predict(X_test)
-ids = test.as_matrix(columns=test.columns[:1])
-ids = pd.DataFrame(ids)
-labels = pd.DataFrame(lr_preds)
-merged = pd.concat([ids,labels], axis=1, keys=['id','label'])
-merged.to_csv("predictions.csv")
-print("Done")
+X_validation = vectorizer.transform(test['Stems'].values.astype('U'))
+preds = model.predict(X_validation)
+df = test['id'].to_frame()
+df['label'] = preds
+print(df.head())
+df.to_csv("preds3.csv", index=False)
+print("Written")
